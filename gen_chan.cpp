fc46981bbeef21ed017bb2a40bf276c05a15cc96
Aditya Arun Kumar <adityaarunkumarphi@gmail.com>
2022-01-16 00:41:03 +0530

    Updated gen_chan.cpp
    Updated gen_chan to have real and imaginary components of the matrix and store.

--- gen_chan.cpp
+++ gen_chan.cpp
@@ -7,20 +7,40 @@ using namespace std;
 using namespace std:: complex_literals;
 
 int m;
-// Matric generation function here
-double chanGen(int n){
-    srand(time(nullptr));
-    complex<double> CMtx;
-    for (int k = 0; k < n; ++k)
+
+void displayMtx(float *arr, int m){
+    for (int i = 0; i < m; ++i)
     {
-        for (int l = 0; l < n; ++l)
+        for (int j = 0; j < m; ++j)
         {
-            complex<double> CMtx(cos(1+rand()/(100000000)), sin(1+rand()/(100000000)));
-            cout << CMtx << '\t';
+            cout << *((arr+i*m)+j) << "\t";
         }
         cout << endl;
     }
     
+}
+
+// Matric generation function here
+float chanGen(int n){
+    srand(time(nullptr));
+    // change the matrix to real and complex matrices separately and to float
+    float H_real[m][m], H_imag[m][m];
+    for (int k = 0; k < n; ++k)
+    {
+        for (int l = 0; l < n; ++l)
+        {
+            complex<float> CMtx(cos(1+rand()/(100000000)), sin(1+rand()/(100000000)));
+            cout << CMtx << '\t';
+            H_real[k][l] = CMtx.real();
+            H_imag[k][l] = CMtx.imag();
+        }
+        cout << endl;
+    }
+    cout << "Channel Matrix Real Components" << endl;
+    displayMtx((float *)H_real, m);
+    cout << "Channel Matrix Imaginary Components" << endl;
+    displayMtx((float *)H_imag, m);
+    return H_real[m][m], H_imag[m][m];
 }
 
 int main(){
